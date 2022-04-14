ff8e0c359bdb4b35396fb6721e8dd62c49219bb6
Christiandiaz4340 <christiandiaz4340@icloud.com>
2022-04-14 16:04:55 -0400

    1 file modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -10,5 +10,18 @@ export class CartService {
 
   items: Product[] = [];
 
+  addToCart(product: Product) {
+    this.items.push(product);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+
   constructor() { }
 }
